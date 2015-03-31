3fff3b0b85cb03133c4f047eba6b5e74d894daa7
Lonnia <cath.lonnia@gmail.com>
2015-04-01 01:11:05 +0900

    Update board.php

--- board.php
+++ board.php
@@ -1,31 +1,31 @@
 <?
-
 $servername = "localhost";
-$username = "/*your databse id*/";
-$password = "/*your databse password*/";
+$username = "/*your database username*/";
+$password = "/*your database password*/";
 
 // Create connection
-$conn = new mysqli($servername, $username, $password);
+$conn = mysql_connect($servername, $username, $password);
+mysql_select_db('/*your database name*/', $conn);
 
 // Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+if (!$conn) {
+    die("Connection failed: " . mysql_error() );
 } 
-echo "Connected successfully";
 
 
-if($_POST['uploader']!='' && $_POST['contents']!=''){
-     // 쿼리문 작성
-    $query = "insert into board (uploader, contents, uptime) values ('$uploader', '$contents', 'date('Y-m-d H:i:s')')";
-    // 쿼리문 적용
-    mysql_query($query, $connect);
+if($_POST['uploader'] && $_POST['contents']){
+    $uploader = $_POST['uploader'];
+    $contents = $_POST['contents'];
+    $uptime = date('Y-m-d H:i:s');
+
+
+    $sql = "insert into board (uploader, contents, uptime) values (\"$uploader\", \"$contents\", \"$uptime\" )";
+    
+    // 쿼리문 작성/적용
+    mysql_query($sql, $conn);
     // 디비 닫기
-    mysql_close($connect);
+    mysql_close($conn);
 }
-
-$sql = "SELECT * FROM board";
-$result = $conn->query($sql);
-
 ?>
 <form name="iForm" method="post">
     <table style="width:700px;height:50px;border:5px #CCCCCC solid;">
@@ -35,17 +35,17 @@ $result = $conn->query($sql);
     </table>
     <table style="width:700px;height:50px;border:0px;">
         <tr>
-        <td align="center" valign="middle" style="width:100px;height:50px;">
-            <input type="text" id="uploader" name="uploader" style="width:300px;" placeHolder="Uploader">
-        </td> 
-        <td align="center" valign="middle" style="width:100px;height:50px;">
-            <input type="text" id="contents" name="contents" style="width:300px;" placeHolder="Contents">
-        </td>
-        <td align="center" valign="middle" style="width:50px;height:50px;">
-            <input type="submit" value="저장">
-        </td>
-    </tr>
-</table>
+            <td align="center" valign="middle" style="width:100px;height:50px;">
+                <input type="text" id="uploader" name="uploader" style="width:300px;" placeHolder="Uploader">
+            </td> 
+            <td align="center" valign="middle" style="width:100px;height:50px;">
+                <input type="text" id="contents" name="contents" style="width:300px;" placeHolder="Contents">
+            </td>
+            <td align="center" valign="middle" style="width:50px;height:50px;">
+                <input type="submit" value="저장">
+            </td>
+        </tr>
+    </table>
 </form>
 
 
@@ -57,29 +57,31 @@ $result = $conn->query($sql);
         <td style="padding:5px 5px 5px 5px;">Uploaded Time</td>
     </tr>
     <?
-    if ($result->num_rows < 0) {
-
-        ?>
-        <tr align="center" valign="middle" style="height:1px;background-color:#CCCCCC;">
-            <td></td>
-            <td></td>
-            <td></td>
-        </tr>
-        <tr align="center" valign="middle" style="height:1px;">
-            <td>NULL</td>
-            <td>NULL</td>
-            <td>NULL</td>
-        </tr>
-        <?}else{?>
-        <tr align="center" valign="middle" style="height:1px;background-color:#CCCCCC;">
-            <td></td>
-            <td></td>
-            <td></td>
-        </tr>
-        <tr align="center" valign="middle" style="font-size:15px;">
-            <td style="padding:5px 5px 5px 5px;"><?=$result['uploader']?></td>
-            <td style="padding:5px 5px 5px 5px;"><?=$result['contents']?></td>
-            <td style="padding:5px 5px 5px 5px;"><?=$result['uptime']?></td>
-        </tr>
-        <?}?>
-    </table>
+    $sql = mysql_query("SELECT * FROM board", $conn);
+    //fetch tha data from the database
+    while ($result = mysql_fetch_array($sql)) {
+        if ($result=='') { ?>
+            <tr align="center" valign="middle" style="height:1px;background-color:#CCCCCC;">
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr align="center" valign="middle" style="height:1px;">
+                <td>NULL</td>
+                <td>NULL</td>
+                <td>NULL</td>
+            </tr>
+            <?}else{?>
+            <tr align="center" valign="middle" style="height:1px;background-color:#CCCCCC;">
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr align="center" valign="middle" style="font-size:15px;">
+                <td style="padding:5px 5px 5px 5px;"><?=$result['uploader']?></td>
+                <td style="padding:5px 5px 5px 5px;"><?=$result['contents']?></td>
+                <td style="padding:5px 5px 5px 5px;"><?=$result['uptime']?></td>
+            </tr>
+        <?}
+    }?>
+</table>
